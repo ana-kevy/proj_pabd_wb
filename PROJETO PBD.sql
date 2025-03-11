a86254aec69aebead0c2aaa98f8cd74ec77bf442
ana-kevy <anakevyllycavalcantec@gmail.com>
2025-03-11 00:11:16 -0300

    Delete PROJETO PBD.sql

--- PROJETO PBD.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS Mortos(
-	id_morto INT NOT NULL,
-	andamento_rito VARCHAR(20) NOT NULL,
-	causa_morte VARCHAR(30) NOT NULL,
-	user_id INT NOT NULL,
-	PRIMARY KEY (id_morto),
-	FOREIGN KEY (user_id) REFERENCES cadastro(user_id)
-)
